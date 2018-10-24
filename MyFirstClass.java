812398b74c082b7773b1f10753df11fa11a09dbc
jyoshnajagarapu <jagarapujyothsna@gmail.com>
2018-10-24 11:41:34 +0530

    third push

---
+++ MyFirstClass.java
@@ -0,0 +1,12 @@
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		//int x = 0x905;;		
+		System.out.println("hello");
+		//System.out.println((char)x);
+		// TODO Auto-generated method stub
+
+	}
+
+}
